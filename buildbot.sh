c76c73b530a3c5787bbac67c2c9f9359d86545c0
Andreas Neumeier <andreas@neumeier.org>
2021-10-20 11:42:47 +0200

    Update buildbot.sh

--- buildbot.sh
+++ buildbot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pushd /home/andreas_neumeier_org/buildbot
+git -c 'core.sshCommand=ssh -o "BatchMode=yes" -i "/home/andreas_neumeier_org/.ssh/id_rsa_buildbot"' pull
 source ../env/bin/activate
 exec buildbot start --nodaemon /home/andreas_neumeier_org/buildbot
 popd
